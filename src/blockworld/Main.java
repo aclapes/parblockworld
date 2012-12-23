9087720f4df14796102cd2f7a39d257f3ff405b6
Albert Clapés <aclapes@gmail.com>
2012-12-23 12:21:36 +0100

    First commit

---
+++ src/blockworld/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blockworld;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * @author aclapes
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, CloneNotSupportedException 
+    {
+//        BlockWorld blockWorld = new BlockWorld("init.txt");
+//        blockWorld.exec();
+    }
+}
